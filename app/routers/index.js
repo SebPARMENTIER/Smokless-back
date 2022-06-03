68fc9af035dd823019a61214a2445f73eef03892
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2022-06-03 14:46:55 +0200

    Add API signature

--- app/routers/index.js
+++ app/routers/index.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+router.get('/', (_, res) => {
+  res.json({
+      message: "API Smok'Less",
+      author: "PARMENTIER Sébastien"
+  });
+});
+
 module.exports = router;
